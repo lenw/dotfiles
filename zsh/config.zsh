55339e481f5e619a559efd3da11594f0d6a019b2
Len Weincier <len@cloudafrica.net>
2012-07-23 09:34:43 +0200

    Config changes. Restore the 'c' alias for zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -16,7 +16,7 @@ HISTSIZE=10000
 SAVEHIST=10000
 
 setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
+#setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
